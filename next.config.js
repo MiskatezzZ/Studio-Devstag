87b10e887b5c836afbad6347d3a3f39369951130
SOUMO <soumo2020.saha@gmail.com>
2025-06-02 22:04:18 +0530

    Simplify Next.js config to minimal configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Very minimal config to allow Vercel to handle the deployment
   reactStrictMode: true,
   images: {
     unoptimized: true
-  },
-  // Keep it simple for Vercel
-  distDir: '.next'
+  }
 };
 
 module.exports = nextConfig;
